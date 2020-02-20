bc93aab774095d2a58a834b723f099e44aa1a390
Bruce Robert Muller <brm512@york.ac.uk>
2020-02-20 13:40:10 +0000

    Create hierarchical_loss.py

---
+++ hierarchical_loss.py
@@ -0,0 +1,31 @@
+
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+def tree_loss(input, target, weight=None, root=None, use_hierarchy=True):    
+    probabilities = F.softmax(cnn_output, dim = 1) ; loss = 0
+
+    for level_loss_list in precomputed_hierarchy_list
+
+        probabilities_tosum = probabilities.clone()
+        summed_probabilities = probabilities_tosum
+        for branch in level_loss_list:
+
+            # Extract the relevant probabilities according to a branch in our hierarchy.
+            branch_probs = torch.FloatTensor()
+            for channel in branch:
+                branch_probs = torch.cat((branch_probs,probabilities_tosum[:,channel,:,:].unsqueeze(1)),1)
+
+            # Sum these probabilities into a single slice; this is hierarchical inference.
+            summed_tree_branch_slice = branch_probs.sum(1,keepdim=True)
+
+            # Insert inferred probability slice into each channel of summed_probabilities given by branch. 
+            # This duplicates probabilities for easy passing to standard loss functions such as nll_loss.
+            for channel in branch:  
+                summed_probabilities[:,channel:(channel+1),:,:] = summed_tree_branch_slice
+
+        level_loss = F.nll_loss(log(summed_probabilities), target)
+        loss = loss + level_loss
+    return(loss)
